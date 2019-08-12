24a5dbbe659be602d617dd5e59b309ec4db59370
Elina Os <elinaos87@gmail.com>
2019-08-12 17:09:44 +0300

    added conf

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
- <head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>to do list</title>
 </head>
 <body>
     <div class="header">
-        <h1>to do list</h1>
+        <h1>TO DO LIST</h1>
     </div>
+    <form>
+        <input type="text" name="task" class="task">
+        <button type="submit" name="submit" class="btn">Add Task</button>
 </body>
 </html>
